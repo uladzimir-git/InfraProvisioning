28f568a20a7f358cf40d0f9d1f09d6a51384a63c
uladzimir-git <75066024+uladzimir-git@users.noreply.github.com>
2020-12-01 23:29:40 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,30 +1,17 @@
 variable "resource_group_location" {
-  default = "westus"
   description = "The location of the resource group"
-}
-
-variable "app_service_plan_name" {
-  default = "tailspin-space-game-asp"
-  description = "The name of the app service plan"
-}
-
-variable "app_service_name_prefix" {
-  default = "tailspin-space-game-web"
-  description = "The beginning part of your App Service host name"
 }
 
 variable "projectName" {
   type = string
 }
 
-
 locals {
   resource_group_name = "rg-${var.projectName}-${lower(terraform.workspace)}"
+  app_service_plan_name = "plan-${var.projectName}-${lower(terraform.workspace)}"
+  app_service_name = "app-${var.projectName}-${lower(terraform.workspace)}"
   
-  vnetName = "vnet-${var.environment}-${var.location}-001"
-  snetName = "snet-${var.environment}-${var.location}-001"
-  nicName  = "nic-01-${local.vmName}-${var.environment}-001"
-  pipName  = "pip-${local.vmName}-${var.environment}-${var.location}-001"
+ 
 }
 
 
